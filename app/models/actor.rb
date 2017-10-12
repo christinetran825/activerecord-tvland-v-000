0d254841ab5cb4e25f36f09d3576c90e7c68016e
Christine Tran <christinetran825@gmail.com>
2017-10-12 06:52:49 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,9 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def self.full_name
+    self.first_name = 
 
+      # actor = Actor.create(:first_name => "Emilia", :last_name => "Clarke")
   end
 
   def self.list_roles
